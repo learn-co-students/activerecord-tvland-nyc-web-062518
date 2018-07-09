dcd202dd3c0aec17a5bbc871575ded42ddd9b26f
grandballoon <luke.sovern.conley@gmail.com>
2018-07-09 14:25:23 -0400

    tvland

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+
+  def build_network(input)
+    channel = Network.new
+    self.network = channel
+    channel.call_letters = input[:call_letters]
+  end
 end
